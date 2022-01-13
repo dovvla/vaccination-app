e0345de5988fafcb8f45461a73f1b207835230d3
Dovla999 <vladimirjovin8@gmail.com>
2022-01-13 03:19:05 +0100

    some utility classes

---
+++ vaccapp/src/main/java/com/timrobot/vaccapp/VaccappApplication.java
@@ -0,0 +1,13 @@
+package com.timrobot.vaccapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VaccappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VaccappApplication.class, args);
+	}
+
+}
---
+++ vaccapp/src/test/java/com/timrobot/vaccapp/VaccappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.timrobot.vaccapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VaccappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
