5ea895f24ef6f80c3c9aba9acd48766dd4b9d90d
Dovla999 <vladimirjovin8@gmail.com>
2022-07-19 08:46:38 +0200

    registration on navbar

--- vaccapp/src/main/ui/vue/src/views/UnregisteredPage.vue
+++ vaccapp/src/main/ui/vue/src/views/UnregisteredPage.vue
@@ -1,19 +1,17 @@
 <template>
   <div>
     <b-navbar fixed="top" toggleable="lg" type="light" variant="light">
-    <b-navbar-brand href="/">Home Page</b-navbar-brand>
-    <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
-    <b-collapse id="nav-collapse" is-nav>    
-
-      <!-- Right aligned nav items -->
-      <b-navbar-nav class="ml-auto">
-        <b-nav-item right href="/Login">Login</b-nav-item>
-      </b-navbar-nav>
-    </b-collapse>
+      <b-navbar-brand href="/">Home Page</b-navbar-brand>
+      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+      <b-collapse id="nav-collapse" is-nav>
+        <!-- Right aligned nav items -->
+        <b-navbar-nav class="ml-auto">
+          <b-nav-item right href="/register">Login</b-nav-item>
+          <b-nav-item right href="/Login">Login</b-nav-item>
+        </b-navbar-nav>
+      </b-collapse>
     </b-navbar>
 
-    <router-view>
-
-    </router-view>
+    <router-view> </router-view>
   </div>
 </template>
